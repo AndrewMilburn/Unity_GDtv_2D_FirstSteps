999464dd5e4b6ef6b2079050475652987fc961e6
Andrew Milburn <sidbell.069@gmail.com>
2023-04-14 10:00:17 +0100

    Setting Up VS

---
+++ FirstSteps/Assets/PrintToConsole.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        Debug.Log("How many obstacles on Level One???");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
